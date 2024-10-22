51d3d979bdb7f9c9277a3222e8bbbffc54f8d479
Laassel <lassel.giovanni@gmail.com>
2024-10-22 16:59:32 +0200

    code change for bonus feature

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ const lamp = document.getElementById('lamp');
 const pButton = document.getElementById('powerButton'); 
 
 // Creo la funzione che permette il cambio d'immagine tramite il click del bottone 
-pButton.addEventListener('click', function() {
-    // cambio l'immagine della lampadina da spenta a quella accesa
-    lamp.src = './img/yellow_lamp.png';
-    // modifco la scritta del button
-    pButton.textContent = 'Spegni';
+// pButton.addEventListener('click', function() {
+//     // cambio l'immagine della lampadina da spenta a quella accesa
+//     lamp.src = './img/yellow_lamp.png';
+//     // modifco la scritta del button
+//     pButton.textContent = 'Spegni';
 
     // Inserimento di una funzione accendi e spegni
     pButton.addEventListener('click', function(){
-        if(lamp.src.includes('./img/yellow_lamp.png')) {
+        if(lamp.src.includes('yellow_lamp.png')) {
             // la lampadina sarà spenta 
             lamp.src = './img/white_lamp.png';
             pButton.textContent = 'Accendi';
@@ -23,4 +23,4 @@ pButton.addEventListener('click', function() {
 
         }
     })
-});
+// });
