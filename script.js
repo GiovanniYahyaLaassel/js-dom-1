773a650ba4a28e0414543fbb5ae0ce62cb18a58a
Laassel <lassel.giovanni@gmail.com>
2024-10-22 15:37:13 +0200

    Add img-button and creation of variables

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log('Hello World');
+
+// Creo due variabili per lampadina e il button
+const lamp = document.getElementById('lamp');
+const button = document.getElementById('powerButton'); 
+
+console.log(lamp, powerButton)
